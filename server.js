6c194fd18184abc4263345d894a4f81885249cb9
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-31 20:24:04 -0700

    Import the necessary installation to be able to run the application, also created a connection with DB

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 //Intall the dependencies
+const mysql = require('mysql2');
+const inquirer = require("inquirer");
+
+// Connection with MySQL
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    passowrd:'Test1234!',
+    database:"employee_db"
+
+})
