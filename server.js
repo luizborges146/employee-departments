5cac660a771ba03bb5940e75e29c5fbf2f0e6c8e
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-31 21:40:26 -0700

    Create a switch statment that will take the user choice to complete an action

--- server.js
+++ server.js
@@ -10,3 +10,63 @@ const connection = mysql.createConnection({
     database:"employee_db"
 
 })
+
+// Create a Prompt Menu
+
+const menu = () => {
+    insquirer.prompt(
+        {
+            type:"list",
+            name: "menu",
+            message:"What would you like to do?", // check this phrase with Andrew later
+            choices:[
+                "Add an employee",
+                "Add a role",
+                "Add a department",
+                "View employees",
+                "view roles",
+                "View departments",
+                "Update employee role",
+                "Update employee manager",
+                "View the total salary per department",
+                "Exit",
+                
+
+            ]
+        }
+    )
+    .then((response) => {
+        switch (response.menu) {
+            case "Add an employee":
+                break;
+
+            case "Add a role":
+                break;
+            
+            case "Add a department":
+                break;
+
+            case "View employees":
+                break;
+
+            case "view roles":
+                break;
+
+            case "View departments":
+                break;
+
+            case "Update employee role":
+                break;
+
+            case "Update employee manager":
+                break;
+
+            case "View the total salary per department":
+                break;
+
+            case "Exit":
+                break;
+        }
+    })
+}
+
