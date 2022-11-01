aaac7b4ca0e635bd3b941af955e83a44ad580e50
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-31 18:32:13 -0700

    Created the schema.sql file with CREATE DB and EMPLOYEE, ROLE and DEPARTMENT tables

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+-- DROP DATABASE IF EXISTS employee_db;
+-- CREATE DATABASE employee_db;
+
+-- USE employee_db;
+
+-- CREATE TABLE department (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     d_name VARCHAR(25) NOT NULL
+-- );
+
+-- CREATE TABLE employee (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     f_name VARCHAR(20),
+--     l_name VARCHAR(30),
+--     role_id INT,
+--     manager_id INT,
+--     FOREIGN KEY (role_id) REFERENCES role(id),
+--     FOREIGN KEY (manager_id) REFERENCES employee(id)
+-- );
+
+-- CREATE TABLE role (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     title VARCHAR(30) NOT NULL,
+--     salary DECIMAL(10,2),
+--     dpt_id INT,
+--     FOREIGN KEY (dpt_id) REFERENCES department(id)
+
+-- );
+
+SHOW COLUMNS FROM department;
+SHOW COLUMNS FROM employee;
+SHOW COLUMNS FROM role;
